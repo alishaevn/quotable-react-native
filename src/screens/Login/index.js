5983716aaa1982cab2179ed7a423ddaa6a3fdb2f
Alisha Evans <alisha@notch8.com>
2020-03-19 22:19:02 -0700

    refactor the text at the bottom of the login screen

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,133 +1,108 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import {
-  Image,
-  Text,
-  TextInput,
-  View,
+    Image,
+    Text,
+    TextInput,
+    View,
 } from 'react-native'
 
 import Button from '../../components/Button'
+import logo from '../../assets/images/logo.png'
 import styles from './styles'
 
 class Login extends Component {
-  constructor() {
-    super()
-    this.state = {
-      registering: false,
-      button: 'login'
-    }
-  }
-
-  onLogin = () => {
-  const { email, password } = this.state;
-  // firebase.auth().signInWithEmailAndPassword(email, password)
-  //   .catch((error) => {
-  //     const { code, message } = error;
-  //     // The message contains the default Firebase string representation of the error
-  //     if (code === 'auth/wrong-password') {
-  //       alert('Wrong password.');
-  //     } else {
-  //       alert(message);
-  //     }
-  //   });
-}
-
-onRegister = () => {
-  const { email, password } = this.state;
-  // firebase.auth().createUserWithEmailAndPassword(email, password)
-  //   .catch((error) => {
-  //     const { code, message } = error;
-  //     // The message contains the default Firebase string representation of the error
-  //     if (code === 'auth/weak-password') {
-  //       alert('The password is too weak.');
-  //     } else {
-  //       alert(message);
-  //     }
-  //   });
-}
-
-  render() {
-    const { button, registering } = this.state
-    let registerMessage
-    let onPress
-    let text
-
-    if (button === 'Login') {
-      text = 'Login'
-      onPress = () => this.onLogin()
-    } else {
-      text = 'Register'
-      onPress = () => this.onRegister()
+    constructor() {
+        super()
+        this.state = {
+            register: true,
+        }
     }
 
-    if(this.state.registering) {
-      button = (<Button
-          onPress={() => this.onRegister()}
-          title="Register"
-          accessibilityLabel="Register for Quotable"
-          style={styles.loginButton}
-        />)
-        registerMessage = (
-        <Text
-          onPress={() => this.setState({registering: false})}
-          style={styles.registerMessage}
-        >
-          Click here to Login
-        </Text>)
-    } else {
-      button = (<Button
-          onPress={() => this.onLogin()}
-          title="Login"
-          accessibilityLabel="Log in to Quotable"
-          style={styles.loginButton}
-        />)
-      registerMessage = (
-        <Text
-          onPress={() => this.setState({registering: true})}
-          style={styles.registerMessage}
-        >
-          Click here to register
-        </Text>)
+    onLogin = () => {
+        const { email, password } = this.state;
+        console.log('clicked the login button')
+        // firebase.auth().signInWithEmailAndPassword(email, password)
+        //   .catch((error) => {
+        //     const { code, message } = error;
+        //     // The message contains the default Firebase string representation of the error
+        //     if (code === 'auth/wrong-password') {
+        //       alert('Wrong password.');
+        //     } else {
+        //       alert(message);
+        //     }
+        //   });
     }
 
-    return(
-      <View style={styles.loginPage}>
-        <Image
-          source={require('../../assets/images/logo.png')}
-          style={styles.logo} />
+    onRegister = () => {
+        const { email, password } = this.state;
+        console.log('clicked the register button')
+        // firebase.auth().createUserWithEmailAndPassword(email, password)
+        //   .catch((error) => {
+        //     const { code, message } = error;
+        //     // The message contains the default Firebase string representation of the error
+        //     if (code === 'auth/weak-password') {
+        //       alert('The password is too weak.');
+        //     } else {
+        //       alert(message);
+        //     }
+        //   });
+    }
 
-        <Text style={styles.loginText}>Email</Text>
-        <TextInput
-          style={styles.inputField}
-          autoCapitalize={'none'}
-          onChangeText={(email) => this.setState({email})}
-          value={this.state.email}
-        />
+    render() {
+        const { register } = this.state
+        let onPress
+        let option
+        let text
 
-        <Text style={styles.loginText}>Password</Text>
-        <TextInput
-          style={styles.inputField}
-          maxLength={30}
-          secureTextEntry={true}
-          autoCapitalize={'none'}
-          onChangeText={(password) => this.setState({password})}
-          value={this.state.password}
-        />
+        if (register) {
+            onPress = () => this.onRegister()
+            option = 'Click here to Login'
+            text = 'Register'
+        } else {
+            onPress = () => this.onLogin()
+            option = 'Click here to Register'
+            text = 'Login'
+        }
 
-        <Button
-          onPress={onPress}
-          text={text}
-        />
-        { registerMessage }
-
-      </View>
-    )
-  }
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={logo}
+                    style={styles.logo}
+                />
+                <Text style={styles.loginText}>
+                    Email
+                </Text>
+                <TextInput
+                    style={styles.inputField}
+                    autoCapitalize={'none'}
+                    onChangeText={(email) => this.setState({ email })}
+                    value={this.state.email}
+                />
+                <Text style={styles.loginText}>
+                    Password
+                </Text>
+                <TextInput
+                    style={styles.inputField}
+                    maxLength={30}
+                    secureTextEntry={true}
+                    autoCapitalize={'none'}
+                    onChangeText={(password) => this.setState({ password })}
+                    value={this.state.password}
+                />
+                <Button
+                    onPress={onPress}
+                    text={text}
+                />
+                <Text
+                    onPress={() => this.setState({ register: !register })}
+                    style={styles.optionText}
+                >
+                    {`${option}`}
+                </Text>
+            </View>
+        )
+    }
 }
 
 export default Login
-
-/*
-Resources:
-  - https://facebook.github.io/react-native/docs/components-and-apis.html
-*/
