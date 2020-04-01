cecbc081228010bdfaad5e443345add161d7d7c0
Alisha Evans <alisha@notch8.com>
2020-03-31 22:48:03 -0700

    refactored app.js to user context

--- App.js
+++ App.js
@@ -1,35 +1,18 @@
-import React, { Component } from 'react'
-import AsyncStorage from '@react-native-community/async-storage'
+import React from 'react'
 
+import { AuthContextProvider, UserContextProvider } from './src/contexts'
 import Login from './src/screens/Login'
 import Tabs from './src/navigation'
 
-export default class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            user: null,
-        }
-    }
-
-    async componentDidMount() {
-        try {
-            const user = await AsyncStorage.getItem('user')
-            this.setState({ user })
-        } catch(error) {
-            console.log('failed to load the "user" from asyncstorage because:', error)
-        }
-    }
-
-    updateState = user => this.setState({ user })
-
-    render() {
-        const { user } = this.state
-
-        return (
-            user
-                ? <Tabs updateState={this.updateState} />
-                : <Login updateState={this.updateState} />
-        )
-    }
+const App = () => {
+    return (
+        <AuthContextProvider>
+            <UserContextProvider>
+                <Tabs />
+                <Login />
+            </UserContextProvider>
+        </AuthContextProvider>
+    )
 }
+
+export default App
