48dfc070194712ec1c1467ccf3f1703594ca337b
Alisha Evans <alisha@notch8.com>
2020-03-31 23:27:34 -0700

    able to login in again

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,20 +1,70 @@
 import React, { Component, createContext } from 'react'
+import { Alert } from 'react-native'
+import axios from 'axios'
 
 class AuthContextProvider extends Component {
     state = {
+        email: '',
         isAuthenticated: false,
+        name: '',
+        password: '',
+        registering: false,
+        token: '',
         username: '',
     }
 
-    toggleAuth = () => {
-        this.setState({ isAuthenticated: !this.state.isAuthenticated })
+    toggleAuth = value => this.setState({ [`${value}`]: !this.state[`${value}`] })
+
+    updateUser = (key, value) => this.setState({ [`${key}`]: value })
+
+    onLogin = () => {
+        const { password } = this.state
+        let { email } = this.state
+
+        if (!email || !password) {
+            Alert.alert('Please fill in all fields!')
+        }
+        email = email.toLowerCase()
+        // debugger
+
+        axios.post('http://localhost:3000/auth/login', {
+            email,
+            password,
+        }, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(async response => {
+                // debugger
+                const {
+                    email,
+                    name,
+                    token,
+                    username,
+                } = response.data
+
+                try {
+                    this.updateUser('email', email)
+                    this.updateUser('name', name)
+                    this.updateUser('token', token)
+                    this.updateUser('username', username)
+                    this.toggleAuth('isAuthenticated')
+                } catch (error) {
+                    console.log('failed to save the user or token to async storage because:', error)
+                }
+            })
+            .catch(error => console.log('failed to log the user in because:', error))
     }
 
     render() {
         return (
             <AuthContext.Provider value={{
                 ...this.state,
+                onLogin: this.onLogin,
                 toggleAuth: this.toggleAuth,
+                updateUser: this.updateUser,
             }}>
                 {this.props.children}
             </AuthContext.Provider>
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,144 +1,46 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import {
-    Alert,
     Image,
     StatusBar,
     Text,
     View,
 } from 'react-native'
-import AsyncStorage from '@react-native-community/async-storage'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import axios from 'axios'
 
+import { AuthContext } from '../../contexts'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
 import logo from '../../assets/images/logo.png'
 import styles from './styles'
 
-class Login extends Component {
-    constructor() {
-        super()
-        this.state = {
-            email: '',
-            name: '',
-            password: '',
-            register: true,
-            username: '',
-        }
+const Login = () => {
+    const {
+        email,
+        isAuthenticated,
+        name,
+        onLogin,
+        password,
+        registering,
+        toggleAuth,
+        updateUser,
+        username,
+    } = useContext(AuthContext)
+
+    let onPress
+    let option
+    let text
+
+    if (registering) {
+        onPress = () => this.onRegister()
+        option = 'Click to login instead'
+        text = 'Register'
+    } else {
+        onPress = () => onLogin()
+        option = 'Click to register instead'
+        text = 'Login'
     }
 
-    onLogin = () => {
-        const { password } = this.state
-        let { email } = this.state
-        const { updateState } = this.props
-
-        if (!email || !password) {
-            Alert.alert('Please fill in all fields!')
-        }
-        email = email.toLowerCase()
-
-        axios.post('http://localhost:3000/auth/login', {
-            email,
-            password,
-        }, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        })
-            .then(async response => {
-                const {
-                    email,
-                    name,
-                    token,
-                    username,
-                } = response.data
-                const user = {
-                    email,
-                    name,
-                    username,
-                }
-
-                try {
-                    await AsyncStorage.setItem('user', JSON.stringify(user))
-                    await AsyncStorage.setItem('token', JSON.stringify(token))
-                    updateState(user)
-                } catch (error) {
-                    console.log('failed to save the user or token to async storage because:', error)
-                }
-            })
-            .catch(error => console.log('failed to log the user in because:', error))
-    }
-
-    onRegister = () => {
-        const {
-            name,
-            password,
-            username,
-        } = this.state
-        let { email } = this.state
-        const { updateState } = this.props
-
-        if (!email || !name || !password || !username) {
-            Alert.alert('Please fill in all fields!')
-        }
-        email = email.toLowerCase()
-
-        axios.post('http://localhost:3000/users', {
-            email,
-            name,
-            password,
-            username,
-        }, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        })
-            .then(async response => {
-                const {
-                    email,
-                    name,
-                    username,
-                } = response.data
-                const user = {
-                    email,
-                    name,
-                    username,
-                }
-
-                try {
-                    await AsyncStorage.setItem('user', JSON.stringify(user))
-                    updateState(user)
-                } catch (error) {
-                    console.log('failed to save the user to async storage because:', error)
-                }
-            })
-            .catch(error => console.log('failed to create the user because', error))
-    }
-
-    render() {
-        const {
-            email,
-            name,
-            password,
-            register,
-            username,
-        } = this.state
-        let onPress
-        let option
-        let text
-
-        if (register) {
-            onPress = () => this.onRegister()
-            option = 'Click to login instead'
-            text = 'Register'
-        } else {
-            onPress = () => this.onLogin()
-            option = 'Click to register instead'
-            text = 'Login'
-        }
-
+    if (!isAuthenticated) {
         return (
             <KeyboardAwareScrollView
                 resetScrollToCoords={{ x: 0, y: 0 }}
@@ -153,7 +55,7 @@ class Login extends Component {
                 <Input
                     autoCapitalize={'none'}
                     maxLength={50}
-                    onChangeText={value => this.setState({ email: value })}
+                    onChangeText={value => updateUser('email', value)}
                     keyboardType='email-address'
                     placeholder='Email'
                     secureTextEntry={false}
@@ -165,20 +67,20 @@ class Login extends Component {
                 <Input
                     autoCapitalize={'none'}
                     maxLength={20}
-                    onChangeText={value => this.setState({ password: value })}
+                    onChangeText={value => updateUser('password', value)}
                     placeholder='Password'
                     secureTextEntry={true}
                     size={.75}
                     value={password}
-                    returnKeyType={register ? 'next' : 'done'}
-                    textContentType={register ? 'newPassword' : 'password'}
+                    returnKeyType={registering ? 'next' : 'done'}
+                    textContentType={registering ? 'newPassword' : 'password'}
                 />
-                {register &&
+                {registering &&
                     <View style={styles.inputWrapper}>
                         <Input
                             autoCapitalize={'words'}
                             maxLength={20}
-                            onChangeText={value => this.setState({ name: value })}
+                            onChangeText={value => updateUser('name', value)}
                             placeholder='First Name'
                             secureTextEntry={false}
                             size={.35}
@@ -189,7 +91,7 @@ class Login extends Component {
                         <Input
                             autoCapitalize={'none'}
                             maxLength={20}
-                            onChangeText={value => this.setState({ username: value })}
+                            onChangeText={value => updateUser('username', value)}
                             placeholder='Username'
                             secureTextEntry={false}
                             size={.35}
@@ -204,7 +106,7 @@ class Login extends Component {
                     text={text}
                 />
                 <Text
-                    onPress={() => this.setState({ register: !register })}
+                    onPress={() => toggleAuth('registering')}
                     style={styles.optionText}
                 >
                     {`${option}`}
@@ -212,6 +114,175 @@ class Login extends Component {
             </KeyboardAwareScrollView>
         )
     }
+
+    return null
 }
+
+
+
+// class Login extends Component {
+
+
+//     onRegister = () => {
+//         const {
+//             name,
+//             password,
+//             username,
+//         } = this.state
+//         let { email } = this.state
+//         const { updateState } = this.props
+
+//         if (!email || !name || !password || !username) {
+//             Alert.alert('Please fill in all fields!')
+//         }
+//         email = email.toLowerCase()
+
+//         axios.post('http://localhost:3000/users', {
+//             email,
+//             name,
+//             password,
+//             username,
+//         }, {
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json',
+//             }
+//         })
+//             .then(async response => {
+//                 const {
+//                     email,
+//                     name,
+//                     username,
+//                 } = response.data
+//                 const user = {
+//                     email,
+//                     name,
+//                     username,
+//                 }
+
+//                 try {
+//                     await AsyncStorage.setItem('user', JSON.stringify(user))
+//                     updateState(user)
+//                 } catch (error) {
+//                     console.log('failed to save the user to async storage because:', error)
+//                 }
+//             })
+//             .catch(error => console.log('failed to create the user because', error))
+//     }
+
+
+//     render() {
+//         const { isAuthenticated, toggleAuth } = useContext(AuthContext)
+//         const {
+//             email,
+//             name,
+//             password,
+//             token,
+//             username,
+//         } = useContext(UserContext)
+//         console.log('context values::', {
+//             email,
+//             name,
+//             password,
+//             token,
+//             username,
+//             isAuthenticated,
+//             toggleAuth,
+//         })
+//         // const {
+//         //     email,
+//         //     name,
+//         //     password,
+//         //     register,
+//         //     username,
+//         // } = this.state
+//         // let onPress
+//         // let option
+//         // let text
+
+//         // if (register) {
+//         //     onPress = () => this.onRegister()
+//         //     option = 'Click to login instead'
+//         //     text = 'Register'
+//         // } else {
+//         //     onPress = () => this.onLogin()
+//         //     option = 'Click to register instead'
+//         //     text = 'Login'
+//         // }
+
+//         return (
+//             <KeyboardAwareScrollView
+//                 resetScrollToCoords={{ x: 0, y: 0 }}
+//                 contentContainerStyle={styles.container}
+//                 scrollEnabled={false}
+//             >
+//                 <StatusBar barStyle='light-content' />
+//                 <Image
+//                     source={logo}
+//                     style={styles.logo}
+//                 />
+//                 <Input
+//                     autoCapitalize={'none'}
+//                     maxLength={50}
+//                     onChangeText={value => this.setState({ email: value })}
+//                     keyboardType='email-address'
+//                     placeholder='Email'
+//                     secureTextEntry={false}
+//                     size={.75}
+//                     value={email}
+//                     returnKeyType='next'
+//                     textContentType='emailAddress'
+//                 />
+//                 <Input
+//                     autoCapitalize={'none'}
+//                     maxLength={20}
+//                     onChangeText={value => this.setState({ password: value })}
+//                     placeholder='Password'
+//                     secureTextEntry={true}
+//                     size={.75}
+//                     value={password}
+//                     returnKeyType={register ? 'next' : 'done'}
+//                     textContentType={register ? 'newPassword' : 'password'}
+//                 />
+//                 {register &&
+//                     <View style={styles.inputWrapper}>
+//                         <Input
+//                             autoCapitalize={'words'}
+//                             maxLength={20}
+//                             onChangeText={value => this.setState({ name: value })}
+//                             placeholder='First Name'
+//                             secureTextEntry={false}
+//                             size={.35}
+//                             value={name}
+//                             returnKeyType='next'
+//                             textContentType='givenName'
+//                         />
+//                         <Input
+//                             autoCapitalize={'none'}
+//                             maxLength={20}
+//                             onChangeText={value => this.setState({ username: value })}
+//                             placeholder='Username'
+//                             secureTextEntry={false}
+//                             size={.35}
+//                             value={username}
+//                             returnKeyType='done'
+//                             textContentType='username'
+//                         />
+//                     </View>
+//                 }
+//                 <Button
+//                     onPress={onPress}
+//                     text={text}
+//                 />
+//                 <Text
+//                     onPress={() => this.setState({ register: !register })}
+//                     style={styles.optionText}
+//                 >
+//                     {`${option}`}
+//                 </Text>
+//             </KeyboardAwareScrollView>
+//         )
+//     }
+// }
 
 export default Login
